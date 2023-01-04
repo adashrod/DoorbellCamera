7d71179635c746efec6bd56a94cea783a52b5262
Doorbell <adashrod@gmail.com>
2023-01-03 19:32:35 -0800

    rename some scripts

--- script/button-handler.sh
+++ script/button-handler.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 scriptDir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+haServerIp=`cat $scriptDir/../haServerIp.txt`
+haServerPort=`cat $scriptDir/../haServerPort.txt`
+webhookId=`cat $scriptDir/../webhookIdPress.txt`
 
-python3 src/main.py `cat $scriptDir/../haServerIp.txt`:`cat $scriptDir/../haServerPort.txt` `cat $scriptDir/../webhookIdPress.txt`
-
+python3 src/rpi-main.py ${haServerIp}:${haServerPort} ${webhookId}
